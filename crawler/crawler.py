2730744e0de618ff14503bde99c977fcaecb87ed
stardust <stardust.platinum.the.world@gmail.com>
2023-07-19 16:45:46 +0900

    Crawler updated.DB access modified

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,13 +1,17 @@
 import time
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver import ActionChains
 
-def crawl_set(set_id, headers):
-    try:
-        req = requests.get("https://www.brickeconomy.com/search?query={}".format(set_id), headers=headers)
-    except requests.exceptions.RequestException:
-        return None
-    bs = BeautifulSoup(req.text, "html.parser")
+def crawl_set(set_id):
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.brickeconomy.com/search?query={}".format(set_id))
+    time.sleep(1)
+    bs = BeautifulSoup(driver.page_source, "html.parser")
     try:
         table = bs.find(id="ContentPlaceHolder1_ctlSetsOverview_GridViewSets")
         if table.find("td", {"class":{"ctlsets-left"}}).div.get_text()[:5] == set_id:    
@@ -35,14 +39,51 @@ def crawl_set(set_id, headers):
     }
     return lego_set_row
 
-def crawl_item_from_paypay(set_id, headers):
+def crawl_item_from_mercari(set_id):
     lego_item_rows = []
     uncrawled_page = True
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://jp.mercari.com/search?keyword=LEGO%20{}&item_condition_id=1&status=on_sale".format(set_id))
+    time.sleep(1)
+    bs = BeautifulSoup(driver.page_source, "html.parser")
     try:
-        req = requests.get("https://paypayfleamarket.yahoo.co.jp/search/LEGO%20{}?conditions=NEW&open=1".format(set_id), headers=headers)
-    except requests.exceptions.RequestException:
+        while uncrawled_page:
+            for itm in bs.find_all("a", class_="sc-295d2608-2 cyhDfc"):
+                title = itm.find("div", class_="imageContainer__f8ddf3a2")["aria-label"][:-6]
+                if "互換" in title:
+                    continue
+                price = int("".join(itm.find("span", class_="number__6b270ca7").get_text().split(",")))
+                url = "https://jp.mercari.com" + itm["href"]
+                lego_item_row = {
+                    "set_id": set_id,
+                    "title": title,
+                    "price": price,
+                    "url": url,
+                }
+                lego_item_rows.append(lego_item_row)
+            button = True
+            if button:
+                #driver.find_element(By.CSS_SELECTOR, ".merButton.secondary__01a6ef84.medium__01a6ef84")
+                #ActionChains(driver).click(button).perform()
+                uncrawled_page = False
+            else:
+                uncrawled_page = False
+    except AttributeError:
         return None
-    bs = BeautifulSoup(req.text, "html.parser")
+    driver.close()
+    return lego_item_rows
+
+def crawl_item_from_paypay(set_id):
+    lego_item_rows = []
+    uncrawled_page = True
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://paypayfleamarket.yahoo.co.jp/search/LEGO%20{}?conditions=NEW&open=1".format(set_id))
+    time.sleep(1)
+    bs = BeautifulSoup(driver.page_source, "html.parser")
     try:
         while uncrawled_page:
             for itm in bs.find(id="itm").find_all("a"):
@@ -61,8 +102,81 @@ def crawl_item_from_paypay(set_id, headers):
             last_li = bs.find("ul", class_="sc-2300bd7f-1 kTZkbX").find_all("li")[-1]
             if last_li.get_text()==" 次へ ":
                 time.sleep(10)
-                req = requests.get("https://paypayfleamarket.yahoo.co.jp"+last_li.find("a")["href"], headers=headers)
-                bs = BeautifulSoup(req.text, "html.parser")
+                driver.get("https://paypayfleamarket.yahoo.co.jp"+last_li.find("a")["href"])
+                print("https://paypayfleamarket.yahoo.co.jp"+last_li.find("a")["href"])
+                time.sleep(1)
+                bs = BeautifulSoup(driver.page_source, "html.parser")
+            else:
+                uncrawled_page = False
+    except AttributeError:
+        return None
+    return lego_item_rows
+
+def crawl_item_from_rakuma(set_id):
+    #disallowed in robots.txt
+    lego_item_rows = []
+    uncrawled_page = True
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://fril.jp/s?query=lego%20{}&status=new&transaction=selling".format(set_id))
+    time.sleep(1)
+    bs = BeautifulSoup(driver.page_source, "html.parser")
+    try:
+        while uncrawled_page:
+            for itm in bs.find_all("div", class_="item-box"):
+                title = itm.find("a", class_="link_search_title").find("span").get_text()
+                if "互換" in title:
+                    continue
+                price = int("".join(itm.find("p", class_="item-box__item-price").find_all("span")[1].get_text().split(",")))
+                url = itm.find("a", class_="link_search_title")["href"]
+                lego_item_row = {
+                    "set_id": set_id,
+                    "title": title,
+                    "price": price,
+                    "url": url,
+                }
+                lego_item_rows.append(lego_item_row)
+
+            next_exist = True
+            if next_exist:
+                uncrawled_page = False
+            else:
+                uncrawled_page = False
+    except AttributeError:
+        return None
+    driver.close()
+    return lego_item_rows
+
+def crawl_item_from_yafuoku(set_id):
+    #disallowed in robots.txt
+    lego_item_rows = []
+    uncrawled_page = True
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://auctions.yahoo.co.jp/search/search?p=lego+{}&va=lego+{}&istatus=1&is_postage_mode=1&dest_pref_code=13&exflg=1&b=1&n=50".format(set_id,set_id))
+    time.sleep(1)
+    bs = BeautifulSoup(driver.page_source, "html.parser")
+    try:
+        while uncrawled_page:
+            for itm in bs.find_all("li", class_="Product"):
+                title = itm.find("a", class_="Product__titleLink js-rapid-override js-browseHistory-add").get_text()
+                if "互換" in title:
+                    continue
+                price = int("".join(itm.find("span", class_="Product__priceValue u-textRed").get_text()[:-1].split(",")))
+                url = itm.find("a", class_="Product__titleLink js-rapid-override js-browseHistory-add")["href"]
+                lego_item_row = {
+                    "set_id": set_id,
+                    "title": title,
+                    "price": price,
+                    "url": url,
+                }
+                lego_item_rows.append(lego_item_row)
+            
+            next_exist = True
+            if next_exist:
+                uncrawled_page = False
             else:
                 uncrawled_page = False
     except AttributeError:
--- crawler/run_crawler.py
+++ crawler/run_crawler.py
@@ -1,48 +1,52 @@
 from datetime import datetime
-import pymysql
-from crawler import crawl_set, crawl_item_from_paypay
+from crawler import crawl_set, crawl_item_from_mercari, crawl_item_from_paypay
+import MySQLdb
 
 if __name__=="__main__":
-    headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
     with open("db_settings.txt") as f:
         [db_user, db_pw, db_name,] = [line[:-1] for line in f.readlines() if line != '']
     with open("set_id_list.txt") as f:
         set_id_list = [set_id [:-1] for set_id in f.readlines() if set_id != '']
     for set_id in set_id_list:
-        lego_set_row = crawl_set(set_id, headers)
-        lego_item_rows = crawl_item_from_paypay(set_id, headers)
-        connection = pymysql.connect(host="localhost",
-                               user=db_user,
-                               passwd=db_pw,
-                               database=db_name,
-                               charset="utf8",
-                               cursorclass=pymysql.cursors.DictCursor)
-        "UPDATE DB"
-        with connection:
-            with connection.cursor() as cursor:
-                """
-                UPDATE lego_set
-                """
-                cursor.execute("SELECT COUNT(*) FROM lego_set WHERE set_id={}".format(set_id))
-                if cursor.fetchone()["COUNT(*)"] !=0 :
-                    cursor.execute("UPDATE lego_set SET value='{}', last_updated='{}' WHERE set_id='{}'".format(lego_set_row["value"], datetime.now(), lego_set_row["set_id"]))
+        lego_set_row = crawl_set(set_id)
+        lego_item_rows = []
+        rows = crawl_item_from_mercari(set_id)
+        if rows:
+            lego_item_rows += rows
+        rows = crawl_item_from_paypay(set_id)
+        if rows:
+            lego_item_rows += rows
+        
+        connection = MySQLdb.connect(
+            user=db_user,
+            passwd=db_pw,
+            host='localhost',
+            db=db_name,
+            charset="utf8")
+        cursor = connection.cursor()
+        cursor.execute("SELECT COUNT(*) FROM lego_set WHERE set_id={}".format(set_id))
+        #UPDATE lego_item
+        if cursor.fetchone()[0] != 0:
+            cursor.execute("UPDATE lego_set SET value='{}', last_updated='{}' WHERE set_id='{}'".format(lego_set_row["value"], datetime.now(), lego_set_row["set_id"]))
+        else:
+            cursor.execute("INSERT INTO lego_set VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(lego_set_row["set_id"], lego_set_row["name"], lego_set_row["theme"], 
+                                                                                                                            lego_set_row["year"], lego_set_row["pieces"], lego_set_row["availability"], 
+                                                                                                                            lego_set_row["retail"], lego_set_row["value"], datetime.now()))
+        #UPDATE lego_item
+        if lego_item_rows:
+            for lego_item_row in lego_item_rows:
+                cursor.execute("SELECT * FROM lego_item WHERE url='{}'".format(lego_item_row["url"]))
+                if cursor.fetchone():
+                    cursor.execute("UPDATE lego_item SET title='{}', price='{}', last_updated='{}' WHERE url='{}'".format(lego_item_row["title"], lego_item_row["price"], 
+                                                                                                                            datetime.now(), lego_item_row["url"]))
                 else:
-                    cursor.execute("INSERT INTO lego_set VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(lego_set_row["set_id"], lego_set_row["name"], lego_set_row["theme"], 
-                                                                                                                               lego_set_row["year"], lego_set_row["pieces"], lego_set_row["availability"], 
-                                                                                                                               lego_set_row["retail"], lego_set_row["value"], datetime.now()))
-                """
-                UPDATE lego_item
-                """
-                if lego_item_rows:
-                    for lego_item_row in lego_item_rows:
-                        cursor.execute("SELECT * FROM lego_item WHERE url='{}'".format(lego_item_row["url"]))
-                        if cursor.fetchone():
-                            cursor.execute("UPDATE lego_item SET title='{}', price='{}', last_updated='{}' WHERE url='{}'".format(lego_item_row["title"], lego_item_row["price"], datetime.now(), lego_item_row["url"]))
-                        else:
-                            cursor.execute("INSERT INTO lego_item (set_id, title, price, url, last_updated) VALUES ('{}', '{}', '{}', '{}', '{}')".format(lego_item_row['set_id'], lego_item_row['title'],
-                                                                                                                                                            lego_item_row['price'], lego_item_row['url'],
-                                                                                                                                                            datetime.now()))
-                "DELETE PAST ITEMS"
-                cursor.execute("DELETE FROM lego_item WHERE last_updated < NOW() - INTERVAL 1 DAY")
-            connection.commit()
+                    cursor.execute("INSERT INTO lego_item (set_id, title, price, url, last_updated) VALUES ('{}', '{}', '{}', '{}', '{}')".format(lego_item_row['set_id'], lego_item_row['title'],
+                                                                                                                                                    lego_item_row['price'], lego_item_row['url'],
+                                                                                                                                                    datetime.now()))
+        "DELETE PAST ITEMS"
+        cursor.execute("DELETE FROM lego_item WHERE last_updated < NOW() - INTERVAL 1 DAY")
+        connection.commit()
+        cursor.close()
+        connection.close()
+
         print(set_id+" completed")
