6336160ebcb554c9021df5495a16ac97de53ca03
stardust <stardust.platinum.the.world@gmail.com>
2023-07-23 00:14:17 +0900

    add search form

--- items/views.py
+++ items/views.py
@@ -1,15 +1,6 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
-from items.models import LegoItem
-
+#from django.shortcuts import render
 from django.views.generic.list import ListView
-
-def index(request):
-    context = {}
-    context["items"] = LegoItem.objects.all()
-    return render(
-        request, "index.html", context
-    )
+from items.models import LegoItem
 
 class LegoItemListView(ListView):
     model = LegoItem
