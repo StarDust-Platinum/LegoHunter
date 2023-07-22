cada6641874af3023c8a1de4676e3d0791c50c54
stardust <stardust.platinum.the.world@gmail.com>
2023-07-22 23:53:47 +0900

    layout update

--- crawler/test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from crawler import crawl_set, crawl_item_from_mercari, crawl_item_from_paypay
-import MySQLdb
-
-if __name__=="__main__":
-    lego_item_rows = []
-    rows = crawl_item_from_mercari("42056")
-    print(rows)
